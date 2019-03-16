50a3121b6942273c4f9c25e6573432259b23c701
giordifungula <giordifungula@gmail.com>
2019-03-16 14:22:37 +0200

    added save button and linked to 1 function

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
+// Grab the priceOutput Element 
+let priceDisplay = document.getElementById('total');
+let beforeImageContainer = document.getElementById('daysValue');
+// Declare the Price variable
+let COMPACT_PRICE, totalCost;
+// Assign the Value of radio
+let currentValue = 0;
+
 function element(e){
     return document.getElementById(e);
     // === Return Any ID element === 
 }
+
 function getHotel(){
     let hotelValue = element('hotelValue').value;
+    console.log('Value selected is ', hotelValue);
     // === Collects the Hotel Value === //
-    if(hotelValue === 1) {
+    if(hotelValue == 1) {
         // == If Compact is Selected ==
-        let COMPACT_PRICE = 1000;
-    } else if (hotelValue === 2){
-        let COMPACT_PRICE = 2000;
-    } else if (hotelValue === 3) {
-        let COMPACT_PRICE = 3000;
+        COMPACT_PRICE = 1000;
+        alert(COMPACT_PRICE);
+    } else if (hotelValue == 2){
+        COMPACT_PRICE = 2000;
+        // alert(1);
+    } else if (hotelValue == 3) {
+        COMPACT_PRICE = 3000;
+        alert(COMPACT_PRICE);        
     }
-    console.log(hotelValue);
+    console.log(hotelValue,'THE price will be ', COMPACT_PRICE);
 }
-function getDays(){
-    // Collect the number of days
-    let numDays = element('daysValue');
-    let inputNumDays = numDays.childNodes[0].value;
-    console.log(numDays);
-    console.log(inputNumDays);
+function getDays(radio){
+    // alert('Old value:' + currentValue);
+    // alert('New value:' + radio.value);
+    currentValue = radio.value;
+    totalCost = currentValue * COMPACT_PRICE;
+    console.log(' Total Price: ', totalCost);
+    let h1 = document.createElement('h1');
+    h1.setAttribute('class', 'price');
+    h1.textContent = 'The Price will be:' + totalCost;
+    beforeImageContainer.appendChild(h1);
 }
 
 function calculateHotel(){
     // element();
     getHotel();
-    getDays();
-    alert('Calculate done');
 }
+
